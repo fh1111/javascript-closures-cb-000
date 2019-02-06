11cd96b37db74c2c84ffed62808d5a4c772b6c32
Fer Hernandez <fh.1111@gmail.com>
2019-02-06 03:51:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const app = "I don't do much."
-
-
 function fatBastard(meal) {
-  function whatsForDinner() { // whatsForDinner() is an inner function, a closure
-    if (!meal) { // whatsForDinner() uses argument provided to the parent function
+  function whatsForDinner() {
+    if (!meal) {
       console.log('My belly is empty. Woe is me.');
     } else if (meal === 'Mini-Me') {
       console.log('The wee man is in my belly!');
@@ -12,8 +9,8 @@ function fatBastard(meal) {
     }
   }
 
-  function digest() { // digest() is an inner function, a closure
-    meal = undefined; // digest() uses argument provided to the parent function
+  function digest() {
+    meal = undefined;
   }
 
   return {
@@ -21,3 +18,41 @@ function fatBastard(meal) {
     digest
   };
 }
+
+const { whatsForDinner, digest } = fatBastard('ribeye');
+whatsForDinner(); // prints 'I'm eatin' a bit of ribeye! Burp.'
+digest();
+whatsForDinner(); // prints 'My belly is empty. Woe is me.'
+
+function raspyDoorGuy() {
+  const password = 'yarr';
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) {
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Pssht. That won't work.");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  };
+}
+
+const { bribe } = raspyDoorGuy();
+bribe(80); // returns 'yarr'
+console.log(bribe(80));
